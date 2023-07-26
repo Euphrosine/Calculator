c124a370836420f0669b21318a8d92ccedf42767
Euphrosine <euphrosine46@gmail.com>
2023-07-26 07:03:07 -0400

    multiple operations & error handling implemented

--- script.js
+++ script.js
@@ -4,68 +4,80 @@ let firstNumber = null;
 let operator = null;
 
 function appendNumber(num) {
-    if (currentValue === '0') {
-        currentValue = num.toString();
-    } else {
-        currentValue += num.toString();
-    }
-    display.value = currentValue;
+  if (currentValue === '0') {
+    currentValue = num.toString();
+  } else {
+    currentValue += num.toString();
+  }
+  display.value = currentValue;
 }
 
 function setOperator(op) {
-    firstNumber = parseFloat(currentValue);
-    operator = op;
-    currentValue = '0';
+  firstNumber = parseFloat(currentValue);
+  if (operator && firstNumber !== null) {
+    calculate();
+  }
+  firstNumber = parseFloat(currentValue);
+  operator = op;
+  currentValue = '0';
 }
 
 function calculate() {
-    if (operator && firstNumber !== null) {
-        const secondNumber = parseFloat(currentValue);
-        const result = operate(operator, firstNumber, secondNumber);
-        display.value = result;
-        currentValue = result.toString();
-        operator = null;
-        firstNumber = null;
+  if (operator && firstNumber !== null) {
+    const secondNumber = parseFloat(currentValue);
+    if (operator === '/' && secondNumber === 0) {
+      display.value = "Error: Division by zero";
+    } else {
+      const result = operate(operator, firstNumber, secondNumber);
+      display.value = roundResult(result);
+      currentValue = result.toString();
+      operator = null;
+      firstNumber = null;
     }
+  }
+}
+function clearDisplay() {
+  currentValue = '0';
+  display.value = currentValue;
+  firstNumber = null;
+  operator = null;
 }
 
-function clearDisplay() {
-    currentValue = '0';
-    display.value = currentValue;
-    firstNumber = null;
-    operator = null;
+function roundResult(num) {
+  const precision = 10;
+  return Math.round(num * precision) / precision;
 }
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        throw new Error("Division by zero is not allowed.");
-    }
-    return a / b;
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed.");
+  }
+  return a / b;
 }
 
 function operate(operator, num1, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
-        default:
-            throw new Error("Invalid operator.");
-    }
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+    default:
+      throw new Error("Invalid operator.");
+  }
 }
